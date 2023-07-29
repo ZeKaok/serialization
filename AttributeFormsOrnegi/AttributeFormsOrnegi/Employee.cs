3eea9fec19a5710884fd6bf006b5010e5cc14239
zehrahome <zehrakarakaya999@gmail.com>
2023-07-29 12:29:26 +0300

    seriliazed edildi

---
+++ AttributeFormsOrnegi/AttributeFormsOrnegi/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeFormsOrnegi
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime DoB { get; set; }
+        public string Department { get; set; }  
+        public int Salary { get; set; }
+        public string additonalInfo { get; set; }
+
+        public Employee() { }   
+    }
+}
---
+++ AttributeFormsOrnegi/AttributeFormsOrnegi/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace AttributeFormsOrnegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee
+            {
+                Name = ad.Text,
+                Phone = telefon.Text,
+                DoB = tarih.Value,
+                Department = bolum.Text,
+                Salary = Convert.ToInt32(maas.Text),
+                additonalInfo = "Serileşme istemiyoruz"
+
+            };
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            FileStream fsout = new FileStream("employee.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+
+            try
+            {
+                using (fsout)
+                {
+                    xs.Serialize(fsout, emp);
+                    MessageBox.Show("Bilgiler Serialize Edildi");
+
+                    ad.Text = "";
+                    telefon.Text = "";
+                    bolum.Text = "";
+                    maas.Text = "";
+                    tarih.Value = System.DateTime.Now;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+
+            }
+            finally
+            {
+                fsout.Close();
+            }
+        }
+    }
+    }
+
